3a286683c9aeae5ace3d7bdbee1d172566516ee5
Eva Larumbe <evalarumbe@gmail.com>
2019-08-27 10:12:17 +0200

    Remove explanations and call the func from inside console.log

--- fortune-teller.js
+++ fortune-teller.js
@@ -14,33 +14,14 @@
  *     tellFortune(5, 'partnerino', 'someplace', 'best job ever')
  */
 
- // the function signature is an instruction for how the func should be called
 function tellFortune(numberOfChildren, partnerName, geoLocation, jobTitle) {
-    // in the background JS is running something like this:
-    // var numberOfChildren = 5;
-    // var partnerName = 'partnerino';
-    // var geoLocation = 'someplace';
-    // var jobTitle = 'best job ever';
 
-    // you can define variables inside a function,
-    // as long as their names don't conflict withe the params
-    let greeting = 'Howdy';
-    if (partnerName[0] === 'p') {
-        greeting = 'Hello i know what letter your name starts with';
-    }
-
-
-
-    console.log(`${greeting} ${partnerName}`);
+    console.log(numberOfChildren, partnerName, geoLocation, jobTitle);
 }
 
-// test only
-// we think of the function call as 'the user'
-// the function call must follow the instruction set in the function signature
+// From this...
 tellFortune(5, 'partnerino', 'someplace', 'best job ever');
+console.log(/* TODO: add function call here */);
 
-
-
-
-
-// console.log(/* TODO: add function call here */);
+// To this...
+console.log(tellFortune(5, 'partnerino', 'someplace', 'best job ever'));
