a08e89e94c922d60f1efe6d639a49055525de75d
Eva Larumbe <evalarumbe@gmail.com>
2019-08-27 10:09:21 +0200

    Explain how parameters are passed from the user into funcs

--- fortune-teller.js
+++ fortune-teller.js
@@ -13,12 +13,30 @@
  * 
  *     tellFortune(5, 'partnerino', 'someplace', 'best job ever')
  */
+
+ // the function signature is an instruction for how the func should be called
 function tellFortune(numberOfChildren, partnerName, geoLocation, jobTitle) {
-    console.log(numberOfChildren, partnerName, geoLocation, jobTitle);
+    // in the background JS is running something like this:
+    // var numberOfChildren = 5;
+    // var partnerName = 'partnerino';
+    // var geoLocation = 'someplace';
+    // var jobTitle = 'best job ever';
+
+    // you can define variables inside a function,
+    // as long as their names don't conflict withe the params
+    let greeting = 'Howdy';
+    if (partnerName[0] === 'p') {
+        greeting = 'Hello i know what letter your name starts with';
+    }
+
+
+
+    console.log(`${greeting} ${partnerName}`);
 }
 
-// demo only
+// test only
 // we think of the function call as 'the user'
+// the function call must follow the instruction set in the function signature
 tellFortune(5, 'partnerino', 'someplace', 'best job ever');
 
 
