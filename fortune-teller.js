d1fa629b826139a4d40bea28b37da60c7f8c336d
Eva Larumbe <evalarumbe@gmail.com>
2019-08-27 11:35:01 +0200

    Add a return value with some explanations

--- fortune-teller.js
+++ fortune-teller.js
@@ -7,21 +7,31 @@
  * @param {string} geoLocation
  * @param {string} jobTitle
  * 
- * @return {string} TODO: what are we returning?
+ * @return {string} fortune
  *
  * @example
  * 
  *     tellFortune(5, 'partnerino', 'someplace', 'best job ever')
  */
 
-function tellFortune(numberOfChildren, partnerName, geoLocation, jobTitle) {
+// I want this kind of function to just return something,
+// And not have any console.logs (we're only using those for testing)
 
+function tellFortune(numberOfChildren, partnerName, geoLocation, jobTitle) {
+    // Step three: When we're finished testing, delete the console.log
+    // we're examining 4 different expressions here
     console.log(numberOfChildren, partnerName, geoLocation, jobTitle);
+
+    // Step two: what do I need to have ready, to get the result I want?
+    const fortune = `You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numberOfChildren} kids.`;
+
+    // Step one: define what I want returned
+    return fortune;
 }
 
-// From this...
-tellFortune(5, 'partnerino', 'someplace', 'best job ever');
-console.log(/* TODO: add function call here */);
-
-// To this...
+// we're passing 4 expressions to tellFortune
+// we're passing 2 expressions to console.log
+// these are nested function calls
+// as each one is called, it gets pushed onto the top of the call stack
 console.log(tellFortune(5, 'partnerino', 'someplace', 'best job ever'));
+
