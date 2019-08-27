d8b2160afcc24baab92436a9d7eed25504669c9c
Eva Larumbe <evalarumbe@gmail.com>
2019-08-27 09:51:56 +0200

    Add JS Docs to make it clear what the function expects and returns

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,5 +1,28 @@
-function tellFortune() {
-    
+/**
+ * This is a fortune teller function.
+ * Why pay a fortune teller when you can just program your fortune yourself?
+ *
+ * @param {number} numberOfChildren
+ * @param {string} partnerName
+ * @param {string} geoLocation
+ * @param {string} jobTitle
+ * 
+ * @return {string} TODO: what are we returning?
+ *
+ * @example
+ * 
+ *     tellFortune(5, 'partnerino', 'someplace', 'best job ever')
+ */
+function tellFortune(numberOfChildren, partnerName, geoLocation, jobTitle) {
+    console.log(numberOfChildren, partnerName, geoLocation, jobTitle);
 }
 
-console.log();
+// demo only
+// we think of the function call as 'the user'
+tellFortune(5, 'partnerino', 'someplace', 'best job ever');
+
+
+
+
+
+// console.log(/* TODO: add function call here */);
