2198c6f4e195e97722fad8c943b2f88c66fd41e9
Eva Larumbe <evalarumbe@gmail.com>
2019-09-02 17:48:19 +0200

    Begin tic-tac-toe

---
+++ tic-tac-toe.js
@@ -0,0 +1,19 @@
+const board = [
+    [null,  'o',    'x'],
+    ['x',   'o',    'potato'],
+    [null,  null,   null]
+];
+
+const players = ['Juan', 'Marlon'];
+
+console.log();
+// console.log(board);
+// console.log(board[1]);
+// console.log(board[1][2]);
+
+// get p of potato
+console.log(board[1][2][0]);
+
+// console.log(board[0]);
+// console.log(board[1]);
+// console.log(board[2]);
