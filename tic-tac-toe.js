208117f4a5ff7d0a24dd3edbd37219ac2fc3398e
Eva Larumbe <evalarumbe@gmail.com>
2019-09-02 17:50:03 +0200

    Tic-tac-toe: Make some moves

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,39 @@
 const board = [
     [null,  'o',    'x'],
-    ['x',   'o',    'potato'],
+    ['x',   'o',    null],
     [null,  null,   null]
 ];
 
 const players = ['Juan', 'Marlon'];
 
-console.log();
-// console.log(board);
-// console.log(board[1]);
-// console.log(board[1][2]);
 
-// get p of potato
-console.log(board[1][2][0]);
+console.log('Before any moves, the board looks like this:\n');
 
-// console.log(board[0]);
-// console.log(board[1]);
-// console.log(board[2]);
+console.log(board[0]);
+console.log(board[1]);
+console.log(board[2]);
+
+
+// make the first move
+console.log(`\n${players[0]} makes a move!`);
+
+board[2][1] = 'o';
+
+console.log('Now the board looks like this:\n');
+
+console.log(board[0]);
+console.log(board[1]);
+console.log(board[2]);
+
+
+// make the second move
+console.log(`\n${players[1]} makes a move!`);
+
+board[2][2] = 'x';
+
+
+console.log('Now the board looks like this:\n');
+
+console.log(board[0]);
+console.log(board[1]);
+console.log(board[2]);
