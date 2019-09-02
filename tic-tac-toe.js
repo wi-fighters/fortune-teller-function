08095dc6bcbb3a606175f87df366d365ab2a866e
Eva Larumbe <evalarumbe@gmail.com>
2019-09-02 17:51:52 +0200

    Refactor tic-tac-toe: Add a function to print board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,13 +6,15 @@ const board = [
 
 const players = ['Juan', 'Marlon'];
 
+function printBoard() {
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
+}
 
 console.log('Before any moves, the board looks like this:\n');
 
-console.log(board[0]);
-console.log(board[1]);
-console.log(board[2]);
-
+printBoard();
 
 // make the first move
 console.log(`\n${players[0]} makes a move!`);
@@ -21,19 +23,13 @@ board[2][1] = 'o';
 
 console.log('Now the board looks like this:\n');
 
-console.log(board[0]);
-console.log(board[1]);
-console.log(board[2]);
-
+printBoard();
 
 // make the second move
 console.log(`\n${players[1]} makes a move!`);
 
 board[2][2] = 'x';
 
-
 console.log('Now the board looks like this:\n');
 
-console.log(board[0]);
-console.log(board[1]);
-console.log(board[2]);
+printBoard();
