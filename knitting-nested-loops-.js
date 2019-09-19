e003e3110db2bb8c8166bbca87e0247c3ec5e330
Eva Larumbe <evalarumbe@gmail.com>
2019-09-19 11:27:10 +0200

    Start knitting nested loops: set up accumulator pattern

---
+++ knitting-nested-loops-.js
@@ -0,0 +1,23 @@
+// The goal:
+// -----
+// -----
+// -----
+
+// First let's get a single row working
+// The mini-goal:
+// -----
+
+// Three parts of accumulator pattern
+
+// 1. set up a placeholder for the result
+let stitches = '';
+
+// 2. figure out the result
+stitches += '-';
+stitches += '-';
+stitches += '-';
+stitches += '-';
+stitches += '-';
+
+// 3. use the result
+console.log(stitches);
