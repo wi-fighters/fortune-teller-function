fffaa320b789ab15e5ac0972dec2609ea8a0ba07
Eva Larumbe <evalarumbe@gmail.com>
2019-10-15 10:49:04 +0200

    Set up a test array

---
+++ sorting-algorithms.js
@@ -0,0 +1,7 @@
+// set up our test array
+const unsortedArray = [];
+
+for (let i = 0; i < 999; i++) {
+    // generate a random number and add it to unsortedArray
+    unsortedArray.push(Math.floor(Math.random() * 999));
+}
